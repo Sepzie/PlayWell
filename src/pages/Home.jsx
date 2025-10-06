47094df88a362d8bde1bb0f882d4b761f3c98305
AnthonyQR <55644542+AnthonyQR@users.noreply.github.com>
2025-10-06 13:30:48 -0700

    Added placeholder for bar chart on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import "/src/css/Home.css";
+import { Chart as ChartJS } from "chart.js/auto"
+import { Bar } from "react-chartjs-2"
 
 function Home() {
   return (
     <div className="page">
       <h1 className="page-header">Home</h1>
-      <p>You have played for...</p>
-      <h2>13 Hours</h2>
-      <p>today</p>
+      <div className="hours-played">
+        <p>You have played for...</p>
+        <h2>13 Hours</h2>
+        <p>today</p>
+      </div>
+      <div className="bar-chart">
+        <Bar
+          data={{
+            labels: ["Game 1", "Game 2", "Game 3"],
+            datasets: [
+              {
+                label: "Time Played (Minutes)",
+                data: [400, 300, 80],
+                borderRadius: 5,
+                barThickness: 30,
+              },
+            ],
+            
+          }}
+          options={{
+            indexAxis: 'y',
+          }}
+        />
+      </div>
     </div>
   );
 }
