0a6d12dd1e3576f3a8ae2bc2e2455ac016882b0e
AnthonyQR <55644542+AnthonyQR@users.noreply.github.com>
2025-11-02 14:03:08 -0800

    Started Work on Stats Table frontend with temporary data

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import StatsTable from '/src/components/StatsTable.jsx';
+import "/src/css/Stats.css";
 
 function Stats() {
   return (
     <div className="page">
       <h1>Stats</h1>
       <p>View your gaming statistics and insights</p>
+      <div id="stats-table-root">
+        <StatsTable/>
+      </div>
     </div>
   );
 }
