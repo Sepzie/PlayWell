8a26b63e37301829ff32c0c909a0c0ad4a7a93a2
FonseLULW <jalfonsclarito@gmail.com>
2025-11-16 16:22:07 -0800

    Created exit button on settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import "/src/css/Settings.css";
 
 function Settings() {
   return (
     <div className="page">
       <h1>Settings</h1>
       <p>Configure your PlayWell preferences</p>
+      <div className='centered-h'>
+        <button className='exit-button'>Exit App</button>
+      </div>
     </div>
   );
 }
